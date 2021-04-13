5139b8323b8d56fde632eda7c06fd5fe93aa35fe
Sunip K. Mukherjee <snuipkmukherjee@gmail.com>
2021-04-13 13:19:59 -0400

    First commit

---
+++ ncv7718.c
@@ -0,0 +1,173 @@
+#define NCV7718_PRIVATE
+#include "ncv7718.h"
+#undef NCV7718_PRIVATE
+#include <stdio.h>
+#include <string.h>
+
+#define eprintf(str, ...)                               \
+    fprintf(stderr, "%s" str "\n", __func__, ##__VA_ARGS__); \
+    fflush(stderr);
+
+int ncv7718_init(ncv7718 *dev, int bus, int cs, int gpiocs)
+{
+    if (dev == NULL)
+    {
+        eprintf("Fatal error, memory for struct not allocated");
+        return NCV7718_FAILURE;
+    }
+    if (dev->bus == NULL)
+    {
+        eprintf("Fatal error, %p is null in %p for device, exiting", dev->bus, dev);
+        return NCV7718_FAILURE;
+    }
+    dev->bus->bus = bus;
+    dev->bus->cs = cs;
+    if (gpiocs > 0)
+        dev->bus->cs_gpio = gpiocs;
+    else
+    {
+        dev->bus->cs_gpio = -1;
+        dev->bus->cs_internal = 1;
+    }
+    dev->lsb = 0;
+    int status = spibus_init(dev->bus);
+    if (status < 0)
+    {
+        eprintf("Error %d initializing SPI Bus", status);
+        return NCV7718_FAILURE;
+    }
+    return NCV7718_SUCCESS;
+}
+
+void ncv7718_destroy(ncv7718 *dev)
+{
+    if (dev == NULL)
+    {
+        eprintf("Could not close device at %p", dev);
+        return;
+    }
+    dev->out_conf = 0x0; // turn off all switches
+    dev->out_en = 0x0;   // turn off all enables
+    if (ncv7718_exec_output(dev) < 0)
+    {
+        eprintf("Could not turn off outputs");
+        return;
+    }
+    spibus_destroy(dev->bus);
+    return;
+}
+
+int ncv7718_set_output(ncv7718 *dev, int axis, int direction)
+{
+    if (axis < 0 || axis > 3)
+    {
+        eprintf("Invalid axis limit: %d out of range", axis);
+        return NCV7718_FAILURE;
+    }
+    axis <<= 1; // double it
+
+    dev->out_en |= ((uint8_t)(0x3 << axis)); // enable channels
+    dev->out_conf &= ~((uint8_t)(0x3 << axis)); // unset output mode
+    if (direction == 1)
+        dev->out_conf |= (uint8_t)(0x1 << axis); // 1 on, 2 off etc
+    else if (direction == -1)
+        dev->out_conf |= (uint8_t)(0x2 << axis); // 1 off, 2 on etc
+    else if (direction != 0)
+    {
+        eprintf("Invalid direction: %d", direction);
+        return NCV7718_FAILURE;
+    }
+    return NCV7718_SUCCESS;
+}
+
+NCV7718_RETCODE ncv7718_exec_output(ncv7718 *dev)
+{
+    ncv7718_cmd cmd[1];
+    ncv7718_data data[1];
+    memset(cmd, 0x0, sizeof(ncv7718_cmd));
+    memset(data, 0x0, sizeof(ncv7718_data));
+    // write configuration
+    cmd->hben = dev->out_en;
+    cmd->hbcnf = dev->out_conf;
+    // transfer once
+    if (spibus_xfer(dev->bus, cmd->buf, sizeof(ncv7718_cmd)) < 0)
+    {
+        eprintf("Could not send command 0x%04X\n", cmd->data);
+        return NCV7718_FAILURE;
+    }
+    if (spibus_xfer_full(dev->bus, data->buf, sizeof(ncv7718_data), cmd->buf, sizeof(ncv7718_cmd)) < 0)
+    {
+        eprintf("Could not confirm command 0x%04X\n", cmd->data);
+        return NCV7718_FAILURE;
+    }
+    // check for errors
+    if (data->hbcr != cmd->hbcnf)
+    {
+        eprintf("HB Configuration does not match: Out -> 0x%04X | In -> 0x%04X", cmd->data, data->data);
+        return NCV7718_FAILURE;
+    }
+    // check for overload
+    if (data->tw)
+    {
+        eprintf("Thermal warning!");
+        return NCV7718_THERMAL_WARNING;
+    }
+    if (data->psf)
+    {
+        eprintf("Power supply failure!");
+        return NCV7718_PSU_FAIL;
+    }
+    if (data->ocs)
+    {
+        eprintf("Over current shutoff");
+        return NCV7718_OC_SHUTOFF;
+    }
+    if (data->uld)
+    {
+        eprintf("Under load detection");
+        return NCV7718_UNDER_LOAD;
+    }
+    return NCV7718_SUCCESS;
+}
+
+#define UNIT_TEST
+#ifdef UNIT_TEST
+#include <unistd.h>
+#include <signal.h>
+volatile sig_atomic_t done = 0;
+void sighandler(int sig)
+{
+    done = 1;
+}
+int main(int argc, char *argv)
+{
+    if (argc != 3 || argc != 4)
+    {
+        printf("Usage: ncv7718 <SPI Bus> <SPI CS> [<Optional GPIO CS>]\n\n");
+        return 0;
+    }
+    int bus = atoi(argv[1]);
+    int cs = atoi(argv[2]);
+    int gpiocs = -1;
+    if (argc == 4)
+        gpiocs = atoi(argv[3]);
+    signal(SIGINT, &sighandler);
+    ncv7718 hb[1];
+    if (ncv7718_init(hb, bus, cs, gpiocs) != NCV7718_SUCCESS)
+    {
+        eprintf("Init failed!");
+        return 0;
+    }
+    while(!done)
+    {
+        ncv7718_set_output(hb, 0, 1);
+        ncv7718_exec_output(hb);
+        sleep(1);
+        ncv7718_set_output(hb, 0, -1);
+        ncv7718_exec_output(hb);
+        sleep(1);
+    }
+    ncv7718_destroy(hb);
+    return 0;
+}
+#endif
---
+++ ncv7718.h
@@ -0,0 +1,101 @@
+/**
+ * @file ncv7718.h
+ * @author Sunip K. Mukherjee (sunipkmukherjee@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-08
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef NCV7718_H
+#define NCV7718_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "spibus/spibus.h"
+
+typedef enum
+{
+    NCV7718_OC_SHUTOFF = -3,
+    NCV7718_PSU_FAIL = -2,
+    NCV7718_FAILURE = -1,
+    NCV7718_SUCCESS = 1,
+    NCV7718_THERMAL_WARNING = 2,
+    NCV7718_UNDER_LOAD,
+} NCV7718_RETCODE;
+
+typedef struct 
+{
+    spibus bus[1];
+    uint8_t out_en;
+    uint8_t out_conf;
+} ncv7718;
+/**
+ * @brief Initialize an NCV7718 device
+ * 
+ * @param dev ncv7718 struct
+ * @param bus SPI Bus ID
+ * @param cs SPI CS ID
+ * @param gpiocs Positive for valid GPIO used as chipselect, negative for native SPI CS
+ * @return int Positive on success, negative on failure
+ */
+int ncv7718_init(ncv7718 *dev, int bus, int cs, int gpiocs);
+/**
+ * @brief Set output of pairs of hex half bridges
+ * 
+ * @param dev ncv7718 struct
+ * @param axis X, Y, Z pairs (1-2, 3-4, 5-6)
+ * @param direction 0 for disabled, +1 for odd-on, even-off, -1 for even-on, odd-off
+ */
+int ncv7718_set_output(ncv7718 *dev, int axis, int direction);
+/**
+ * @brief Apply the outputs set using ncv7718_set_output
+ * 
+ * @param dev ncv7718 struct
+ * @return int positive on success, negative on failure
+ */
+NCV7718_RETCODE ncv7718_exec_output(ncv7718 *dev);
+/**
+ * @brief Turn off outputs and close an ncv7718 device
+ * 
+ * @param dev ncv7718 struct
+ */
+void ncv7718_destroy(ncv7718 *dev);
+
+#ifdef NCV7718_PRIVATE
+typedef union
+{
+    struct __attribute__((packed))
+    {
+        unsigned char ovlo  : 1; ///< over voltage lockout
+        unsigned char hbcnf : 6; ///< half bridge 1 configuration (1 -> LS1 off and HS1 on, 0 -> LS1 on and HS1 off)
+        unsigned char hben  : 6; ///< half bridge 1 enable (1 -> bridge in use, 0 -> bridge not in use)
+        unsigned char uldsc : 1; ///< under load detection shutdown
+        unsigned char hbsel : 1; ///< half bridge selection (needs to be set to 0)
+        unsigned char srr   : 1; ///< status reset register: 1 -> clear all faults and reset
+    };
+    char buf[2];
+    unsigned short data;
+} ncv7718_cmd;
+
+typedef union
+{
+    struct __attribute__((packed))
+    {
+        unsigned char tw   : 1; ///< Thermal warning
+        unsigned char hbcr : 6; ///< half bridge 1 configuration reporting (1 -> LS1 off and HS1 on, 0 -> LS1 on and HS1 off)
+        unsigned char hbst : 6; ///< half bridge 1 enable (1 -> bridge in use, 0 -> bridge not in use)
+        unsigned char uld  : 1; ///< Under load detection
+        unsigned char psf  : 1; ///< Power supply failure
+        unsigned char ocs  : 1; ///< Overcurrent tripped
+    };
+    char buf[2];
+    unsigned short data;
+} ncv7718_data;
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+#endif
